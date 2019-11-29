046ce08914697730cea3ff271649d5be5ef239fc
ameertg <ameer.saadat99@gmail.com>
2019-11-29 00:47:18 +0000

    Fix minor issues

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -60,7 +60,7 @@ public class App
 		}
 		
 		// Make moves until out of power or taken too many steps write to file for each move
-		String fileName = String.format("/ilp-results/%s-%s-%s-%s.txt", state, day, month, year);
+		String fileName = String.format("ilp-results/%s-%s-%s-%s.txt", state, day, month, year);
 		String line;
 		Direction move;
 		Position before;
@@ -68,7 +68,7 @@ public class App
 		int count = 0;
 		try {
 			FileWriter writer = new FileWriter(fileName);
-			while(drone.power > 0 && count <= 250) {
+			while(drone.power > 0 && count < 250) {
 				before = drone.location;
 				move = drone.makeMove();
 				after = drone.location;
@@ -93,7 +93,7 @@ public class App
 		
 		// Print out geojson and write to file
 		System.out.println(result);
-		fileName = String.format("/ilp-results/%s-%s-%s-%s.geojson", state, day, month, year);
+		fileName = String.format("ilp-results/%s-%s-%s-%s.geojson", state, day, month, year);
 		
 		try {
 			FileWriter writer = new FileWriter(fileName);
